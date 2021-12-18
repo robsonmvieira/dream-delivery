8d8b9681e2f67b16e282aa723580eb917fedfa4f
Robson Maia <robsonmvieira@gmail.com>
2021-12-18 19:24:09 -0300

    :recycle: refactor Listproduct useCase

---
+++ src/application/helpers/index.ts
@@ -0,0 +1,2 @@
+export * from './http-response'
+export * from './help-messages'
--- src/domain/modules/products/usecases/list-product.usecase.ts
+++ src/domain/modules/products/usecases/list-product.usecase.ts
@@ -1,6 +1,6 @@
 import { ProductResponse } from '@/domain/modules/products/dtos'
-import { IListAllProductRepository } from '@/domain/modules/products/ports'
 import { InternalServerError } from '@/domain/modules/core/errors'
+import { IListAllProductRepository } from '@/domain/modules/products/ports'
 export class ListProductsUseCase {
   constructor (private readonly listAllProductRepository: IListAllProductRepository) {}
   async perform (): Promise<ProductResponse[] | InternalServerError> {
