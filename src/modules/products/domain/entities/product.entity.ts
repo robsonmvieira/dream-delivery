e358818bd8262a976408e1daf89358dcfdcae582
Robson Maia <robsonmvieira@gmail.com>
2021-12-18 13:53:06 -0300

    :sparkles: add product entity

---
+++ src/modules/products/domain/entities/product.entity.ts
@@ -0,0 +1,18 @@
+import { Entity } from '@/modules/core/base-classes'
+
+export class Product extends Entity {
+  sku: string
+  name: string
+  price: number
+  category: string
+  categoryId: string
+  description: string
+
+  constructor (data: Partial<Product>) {
+    super()
+
+    if (data !== undefined) {
+      Object.assign(this, data)
+    }
+  }
+}
