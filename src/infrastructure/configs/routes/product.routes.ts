400bac0a0400fa82af01a24d2450209adb6248ae
Robson Maia <robsonmvieira@gmail.com>
2021-12-23 18:48:33 -0300

    ♻️ improve product router structure

--- src/infrastructure/configs/routes/product.routes.ts
+++ src/infrastructure/configs/routes/product.routes.ts
@@ -1,21 +1,9 @@
 import { Router } from 'express'
-// import { listProductControllerFactory } from '@/infrastructure/factories/controllers'
-import { ListProductController } from '@/application/controllers'
-import { ListAllProductPrismaRepository } from '@/infrastructure/repositories/prisma'
-import { ListProductsUseCase } from '@/domain/modules/products/usecases'
-const listAllProductPrismaRepository = new ListAllProductPrismaRepository()
-const useCase = new ListProductsUseCase(listAllProductPrismaRepository)
-
-const listProductController = new ListProductController(useCase)
-// console.log(listProductController.handler())
+import { listProductControllerFactory } from '@/infrastructure/factories/controllers'
 
 const routes = Router()
-// export default (router: Router): void => {
-//   console.log('log acima')
-//   router.get('/', (req, res) => {
-//     console.log('loguei')
-//     res.status(200).json({ data: 'Its ok!!' })
-//   })
-// }
-routes.get('/', listProductController.handler)
+
+routes.get('/', async (req, res) => listProductControllerFactory().handler(req, res))
+routes.post('/', listProductControllerFactory().handler)
+
 export default routes
