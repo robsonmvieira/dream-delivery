103d5aaadfaf28bf160c7efcbdff55943fbc119c
Robson Maia <robsonmvieira@gmail.com>
2021-12-24 00:21:30 -0300

    🔨 update schema database config

--- prisma/migrations/20211223225226_initial_migrate/migration.sql
+++ prisma/migrations/20211223225226_initial_migrate/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE "Product" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "description" TEXT NOT NULL,
-    "price" INTEGER NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
     "category" TEXT NOT NULL,
     "categoryId" TEXT NOT NULL,
     "sku" TEXT NOT NULL,
