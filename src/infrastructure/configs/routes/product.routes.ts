fa712d17e925ff22ac90ca75619cbca4d30a050d
Robson Maia <robsonmvieira@gmail.com>
2021-12-24 00:32:36 -0300

    🎨 add create product router

--- src/infrastructure/configs/routes/product.routes.ts
+++ src/infrastructure/configs/routes/product.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express'
-import { listProductControllerFactory } from '@/infrastructure/factories/controllers'
+import { createProductControllerFactory, listProductControllerFactory } from '@/infrastructure/factories/controllers'
 
 const routes = Router()
 
 routes.get('/', async (req, res) => listProductControllerFactory().handler(req, res))
-routes.post('/', listProductControllerFactory().handler)
+routes.post('/', async (req, res) => createProductControllerFactory().handler(req, res))
 
 export default routes
