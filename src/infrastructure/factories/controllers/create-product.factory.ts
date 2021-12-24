2f65ea80813960849e98abd95ea64472345be30d
Robson Maia <robsonmvieira@gmail.com>
2021-12-24 00:37:34 -0300

    ♻️ update create product controller factory

--- src/infrastructure/factories/controllers/create-product.factory.ts
+++ src/infrastructure/factories/controllers/create-product.factory.ts
@@ -1,11 +1,5 @@
-// import moduleName from '@';
-// import { ListProductsUseCase } from '@/domain/modules/products/usecases'
-
 import { CreateProductController } from '@/application/controllers'
 import { createProductUseCaseFactory } from '@/infrastructure/factories/usecases'
-// import { createProductPrismaRepository } from '@/infrastructure/factories/repositories/products/prisma'
-
-// import { listProductRepositoryFactory } from '@/infrastructure/factories/repositories/products/prisma/typeorm-list-product.factory'
 export const createProductControllerFactory = (): CreateProductController => {
   return new CreateProductController(createProductUseCaseFactory())
 }
