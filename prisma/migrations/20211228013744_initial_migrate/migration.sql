c8a154db87ff0332c4c902e5aecf3e134c7820b8
Robson Maia <robsonmvieira@gmail.com>
2021-12-27 23:38:55 -0300

    :hammer: udapte database script

---
+++ prisma/migrations/20211228013744_initial_migrate/migration.sql
@@ -0,0 +1,47 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "category" TEXT NOT NULL,
+    "categoryId" TEXT NOT NULL,
+    "sku" TEXT NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Supplier" (
+    "id" TEXT NOT NULL,
+    "CNPJ" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "isActive" BOOLEAN NOT NULL,
+    "isBlocked" BOOLEAN NOT NULL,
+    "isDeleted" BOOLEAN NOT NULL,
+
+    CONSTRAINT "Supplier_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Address" (
+    "id" TEXT NOT NULL,
+    "street" TEXT,
+    "number" TEXT,
+    "ZipCode" TEXT,
+    "city" TEXT,
+    "neighborhood" TEXT,
+    "state" TEXT,
+    "moreInfo" TEXT,
+    "supplierId" TEXT NOT NULL,
+
+    CONSTRAINT "Address_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Address_supplierId_key" ON "Address"("supplierId");
+
+-- AddForeignKey
+ALTER TABLE "Address" ADD CONSTRAINT "Address_supplierId_fkey" FOREIGN KEY ("supplierId") REFERENCES "Supplier"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
