0bb0dae2a917b821a2d36e40a57775eef9c413c0
Robson Maia <robsonmvieira@gmail.com>
2021-12-28 22:04:58 -0300

    🔨 add supplier routes config root

--- src/infrastructure/configs/routes.ts
+++ src/infrastructure/configs/routes.ts
@@ -1,9 +1,9 @@
 
 import { Express } from 'express'
-// import { readdirSync } from 'fs'
-// import { join } from 'path'
-// import { listProductControllerFactory } from '../factories/controllers'
 import productRoutes from '@/infrastructure/configs/routes/product.routes'
+import supplierRoutes from '@/infrastructure/configs/routes/suppliers.routes'
+
 export const routesRoot = (app: Express): void => {
   app.use('/api/products', productRoutes)
+  app.use('/api/suppliers', supplierRoutes)
 }
