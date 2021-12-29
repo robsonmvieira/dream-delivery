ad26c237f49dce255f4a9b1f94ec732af4d57160
Robson Maia <robsonmvieira@gmail.com>
2021-12-28 22:07:16 -0300

    🚚 change list Prodc factory to more semantic name

--- src/infrastructure/factories/controllers/list-products.factory.ts
+++ src/infrastructure/factories/controllers/list-products.factory.ts
@@ -1,8 +1,8 @@
 import { ListProductController } from '@/application/controllers'
 import { ListProductsUseCase } from '@/domain/modules/products/usecases'
-import { listProductRepositoryFactory } from '@/infrastructure/factories/repositories/products/prisma/typeorm-list-product.factory'
+import { listProductPrismaRepositoryFactory } from '@/infrastructure/factories/repositories/products/prisma'
 export const listProductControllerFactory = (): ListProductController => {
-  const repo = listProductRepositoryFactory()
+  const repo = listProductPrismaRepositoryFactory()
   const useCase = new ListProductsUseCase(repo)
   return new ListProductController(useCase)
 }
