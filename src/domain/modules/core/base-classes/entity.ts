7cd66f495d71d793bfd9d3d6bf2a7a834007f17c
Robson Maia <robsonmvieira@gmail.com>
2021-12-28 21:59:07 -0300

    ♻️ move common method to base class

--- src/domain/modules/core/base-classes/entity.ts
+++ src/domain/modules/core/base-classes/entity.ts
@@ -1,7 +1,13 @@
+import { ID } from '@/domain/modules/core/value-objects'
+
 export abstract class Entity {
   protected id: string
 
   public getid (): string {
     return this.id
+  }
+
+  protected isValidateId (newId: string): boolean {
+    return ID.validate(newId)
   }
 }
