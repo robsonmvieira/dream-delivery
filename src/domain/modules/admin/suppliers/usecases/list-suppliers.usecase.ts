90216df4aef71b1d82982439cc429e1fecc284b4
Robson Maia <robsonmvieira@gmail.com>
2022-01-05 20:42:14 -0300

    ✨ create list suplliers usecase

---
+++ src/domain/modules/admin/suppliers/usecases/list-suppliers.usecase.ts
@@ -0,0 +1,11 @@
+import { IListSuppliersRepository } from '@/domain/modules/admin/suppliers/ports'
+import { InternalServerError } from '@/domain/modules/core/errors'
+import { SupplierResponse } from '@/domain/modules/admin/suppliers/dtos'
+
+export class ListSuppliersUseCase {
+  constructor (private readonly listSuppliersRepo: IListSuppliersRepository) {}
+
+  async perform (): Promise<SupplierResponse[] | InternalServerError> {
+    return this.listSuppliersRepo.listSuppliers()
+  }
+}
