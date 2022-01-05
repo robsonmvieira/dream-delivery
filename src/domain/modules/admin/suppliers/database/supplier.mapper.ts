9460faa9b1643eee0a83cfe1cb69048e3785d9af
Robson Maia <robsonmvieira@gmail.com>
2022-01-05 20:38:36 -0300

    ♻️ update supplier mapper resource

--- src/domain/modules/admin/suppliers/database/supplier.mapper.ts
+++ src/domain/modules/admin/suppliers/database/supplier.mapper.ts
@@ -4,22 +4,46 @@ export class SupplierMapper {
   static fromPrismaDataBaseToResponse (entity: any): SupplierResponse {
     return {
       id: entity.id,
+      name: entity.name,
       CNPJ: entity.CNPJ,
-      address: {
-        street: entity.address.street,
-        number: entity.address.number,
-        ZipCode: entity.address.zipCode,
-        city: entity.address.city,
-        neighborhood: entity.address.neighborhood,
-        state: entity.address.state,
-        moreInfo: entity.address.moreInfo ?? ''
-      },
       email: entity.email,
       code: entity.code,
       phone: entity.phone,
       isActive: entity.isActive,
       isBlocked: entity.isBlocked,
-      isDeleted: entity.isDeleted
+      isDeleted: entity.isDeleted,
+      address: {
+        street: entity.address.street,
+        number: entity.address.number,
+        zipCode: entity.address.zipCode,
+        city: entity.address.city,
+        neighborhood: entity.address.neighborhood,
+        state: entity.address.state,
+        moreInfo: entity.address.moreInfo ?? ''
+      }
+    }
+  }
+
+  static fromEntityDataBaseToResponse (entity: any): SupplierResponse {
+    return {
+      id: entity.id,
+      name: entity.name,
+      CNPJ: entity.CNPJ,
+      email: entity.email,
+      code: entity.code,
+      phone: entity.phone,
+      isActive: entity.isActive,
+      isBlocked: entity.isBlocked,
+      isDeleted: entity.isDeleted,
+      address: {
+        street: entity.address.street,
+        number: entity.address.number,
+        zipCode: entity.address.zipCode,
+        city: entity.address.city,
+        neighborhood: entity.address.neighborhood,
+        state: entity.address.state,
+        moreInfo: entity.address.moreInfo
+      }
     }
   }
 }
