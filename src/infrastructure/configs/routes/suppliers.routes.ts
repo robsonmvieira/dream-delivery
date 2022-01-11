c62e63d8ab9a27d89ed989663fa288d0b7948434
Robson Maia <robsonmvieira@gmail.com>
2022-01-10 22:46:51 -0300

    🔨 add delete router

--- src/infrastructure/configs/routes/suppliers.routes.ts
+++ src/infrastructure/configs/routes/suppliers.routes.ts
@@ -1,9 +1,11 @@
-import { listSupplierControllerFactory, createSupplierControllerFactory } from '@/infrastructure/factories/controllers/supplier'
+import { listSupplierControllerFactory, createSupplierControllerFactory, deleteSupplierControllerFactory } from '@/infrastructure/factories/controllers/supplier'
+
 import { Router } from 'express'
 
 const routes = Router()
 
 routes.get('/', async (req, res) => listSupplierControllerFactory().handler(req, res))
 routes.post('/', async (req, res) => createSupplierControllerFactory().handler(req, res))
+routes.delete('/:id', async (req, res) => deleteSupplierControllerFactory().handler(req, res))
 
 export default routes
