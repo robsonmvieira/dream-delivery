93c1b902f4955351178baf4d18055f22ac115e31
Robson Maia <robsonmvieira@gmail.com>
2022-01-10 22:42:19 -0300

    🔨 simplefy response data

--- src/application/controllers/create-product.controller.ts
+++ src/application/controllers/create-product.controller.ts
@@ -9,6 +9,6 @@ export class CreateProductController {
     const payload = req.body
     const response = await this.createProductUseCase.perform(payload)
 
-    return response instanceof BadRequestError ? res.status(400).json({ data: response }) : res.status(201).json({ data: response })
+    return response instanceof BadRequestError ? res.status(400).json({ data: response }) : res.status(201).json(response)
   }
 }
--- src/application/controllers/suppliers/create.controller.ts
+++ src/application/controllers/suppliers/create.controller.ts
@@ -10,6 +10,6 @@ export class CreateSupplierController {
     const response = await this.createSupplierUseCase.perform(payload)
     return response instanceof BadRequestError
       ? res.status(400).json({ data: response })
-      : res.status(200).json({ data: response })
+      : res.status(201).json(response)
   }
 }
